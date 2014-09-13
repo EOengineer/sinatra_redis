54c0f3fb99d0e93782725d0dcb2fa39e0815a6ed
Eric Oligney <EOengineer@gmail.com>
2014-09-13 18:20:13 -0400

    added JSON post request.

--- lib/relation.rb
+++ lib/relation.rb
@@ -7,23 +7,34 @@ class Relation < Sinatra::Base
 
 end
 
-
-
-get '/home' do
-  "Hello World"
-end
-
+#index
 get '/relations' do
   @relation = Redis.new(host: '127.0.0.1', port: 6379)
   show = @relation.keys.sort
   "all the keys #{show}"
 end
 
+#read
 get '/relation' do
   content_type :json
   @relation = Redis.new(host: '127.0.0.1', port: 6379)
   object = @relation.get(params[:object])
-  relation_name = @relation.get(params[:r_name])
-  show = {object: "#{object}", relation_name: "#{relation_name}"}
-  show.to_json
+  if !object
+    {error: "no relation object"}.to_json
+  else
+    object
+  end
+end
+
+#create
+post '/relation/new' do
+  content_type :json
+  @relation = Redis.new(host: '127.0.0.1', port: 6379)
+  object = JSON.parse(request.body.read)
+  @relation.set('test8', object.to_json)
+  if @relation.save
+    status 201
+  else
+    status 500
+  end
 end
