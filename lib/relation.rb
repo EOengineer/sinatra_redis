b19145a301a64c5a4317c51cd01d3144417dc990
Eric Oligney <EOengineer@gmail.com>
2014-09-20 21:05:47 -0400

    now parsing json

--- lib/relation.rb
+++ lib/relation.rb
@@ -5,36 +5,61 @@ require 'json'
 
 class Relation < Sinatra::Base
 
-end
+  def get_all_relations(params)
 
-#index
-get '/relations' do
-  @relation = Redis.new(host: '127.0.0.1', port: 6379)
-  show = @relation.keys.sort
-  "all the keys #{show}"
-end
+  end
 
-#read
-get '/relation' do
-  content_type :json
-  @relation = Redis.new(host: '127.0.0.1', port: 6379)
-  object = @relation.get(params[:object])
-  if !object
-    {error: "no relation object"}.to_json
-  else
-    object
+  def check_relation_exists(params)
+
   end
 end
+
+
+
+#get all objects for given relation object
+get '/relations' do
+  content_type :json
+  @relation = Redis.new(host: '127.0.0.1', port: 6379)
+  object_key = params["object_a_type"] + ":" + params["object_a_id"] + ":" + params["relation_name"]
+  object = @relation.get("#{object_key}")
+  if !object
+    status 404, {error: "no relation object"}.to_json
+  else
+    object.to_json
+  end
+end
+
+
+
+get '/relation' do
+  @relation = Redis.new(host: '127.0.0.1', port: 6379)
+  # binding.pry
+  requested_object = params["object_a_type"] + ":" + params["object_a_id"] + ":" + params["relation_name"] + ":" + params["object_b_type"] + ":" + params["obj_b_id"]
+  object = @relation.get(requested_object)
+  if !object
+    status 404, {error: "this relation does not exist"}.to_json
+  else
+    status 200
+  end
+end
+
+
 
 #create
 post '/relation/new' do
   content_type :json
   @relation = Redis.new(host: '127.0.0.1', port: 6379)
+  # binding.pry
   object = JSON.parse(request.body.read)
-  @relation.set('test8', object.to_json)
+  record_key = "#{object["object_a_type"]}:#{object["object_a_id"]}:#{object["relation_name"]}"
+  record_val = "#{object["object_b_type"]}:#{object["object_b_id"]}"
+  @relation.set(record_key, record_val)
+
+
+
   if @relation.save
     status 201
   else
-    status 500
+    status 500, {error: "unprocessible entity"}.to_json
   end
 end
