cb76811507d15ac9cdb9feb8899e6714ca2540d9
Eric Oligney <EOengineer@gmail.com>
2014-09-20 23:31:41 -0400

    now checks if single relation exists.

--- lib/relation.rb
+++ lib/relation.rb
@@ -22,24 +22,24 @@ get '/relations' do
   @relation = Redis.new(host: '127.0.0.1', port: 6379)
   object_key = params["object_a_type"] + ":" + params["object_a_id"] + ":" + params["relation_name"]
   object = @relation.get("#{object_key}")
-  if !object
-    status 404, {error: "no relation object"}.to_json
-  else
+  if object
     object.to_json
+  else
+    status 404, {error: "no relation object"}.to_json
   end
 end
 
 
-
-get '/relation' do
+#checks if a single relation exists
+get '/relation/exists' do
   @relation = Redis.new(host: '127.0.0.1', port: 6379)
-  # binding.pry
-  requested_object = params["object_a_type"] + ":" + params["object_a_id"] + ":" + params["relation_name"] + ":" + params["object_b_type"] + ":" + params["obj_b_id"]
-  object = @relation.get(requested_object)
-  if !object
-    status 404, {error: "this relation does not exist"}.to_json
-  else
+  requested_key = params["object_a_type"] + ":" + params["object_a_id"] + ":" + params["relation_name"]
+  requested_val = params["object_b_type"] + ":" +params["object_b_id"]
+  database_val = @relation.get("#{requested_key}")
+  if (requested_key + ":" +requested_val) == (requested_key + ":" + database_val)
     status 200
+  else
+    status 404
   end
 end
 
@@ -49,14 +49,10 @@ end
 post '/relation/new' do
   content_type :json
   @relation = Redis.new(host: '127.0.0.1', port: 6379)
-  # binding.pry
   object = JSON.parse(request.body.read)
   record_key = "#{object["object_a_type"]}:#{object["object_a_id"]}:#{object["relation_name"]}"
   record_val = "#{object["object_b_type"]}:#{object["object_b_id"]}"
   @relation.set(record_key, record_val)
-
-
-
   if @relation.save
     status 201
   else
